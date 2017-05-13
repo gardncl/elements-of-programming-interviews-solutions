2a573d0ac83bffaf272545a1c31aa2d5bd83ab59
gardncl <gardncl@gmail.com>
2017-05-12 20:47:28 -0400

    9.3

--- stacksandqueues/src/main/java/IsStringWellFormed.java
+++ stacksandqueues/src/main/java/IsStringWellFormed.java
@@ -1,11 +1,41 @@
+import java.util.*;
+
 public class IsStringWellFormed {
 
     /*
     9.3
     */
 
-    public static boolean isWellFormed(String s) {
 
-        return false;
+
+    public static boolean isWellFormed(String s) {
+        Set<Character> left = new HashSet<>(Arrays.asList('(','[','{'));
+        Stack<Character> stack = new Stack<>();
+        for (Character c : s.toCharArray()) {
+            if (left.contains(c)) {
+                stack.push(c);
+            } else {
+                try {
+                    if (c.equals(')')) {
+                        if (!stack.pop().equals('('))
+                            return false;
+                    } else if (c.equals('}')) {
+                        if (!stack.pop().equals('{'))
+                            return false;
+                    } else if (c.equals(']')) {
+                        if (!stack.pop().equals('['))
+                            return false;
+                    } else {
+                        return false;
+                    }
+
+                } catch (EmptyStackException e) {
+                    return false;
+                }
+            }
+        }
+        if (!stack.empty())
+            return false;
+        return true;
     }
 }
