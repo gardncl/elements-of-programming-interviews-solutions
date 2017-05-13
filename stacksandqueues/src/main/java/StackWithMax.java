9036676c2134b15d6ba6e264b2b4cf2dffbbb4e2
gardncl <gardncl@gmail.com>
2017-05-12 20:04:58 -0400

    9.1 solved

--- stacksandqueues/src/main/java/StackWithMax.java
+++ stacksandqueues/src/main/java/StackWithMax.java
@@ -1,22 +1,40 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class StackWithMax {
 
     /*
     9.1
     */
 
+    private Integer max = 0;
+    private List<Integer> stack = new ArrayList<>();
+
     public StackWithMax() {
 
     }
 
     public Integer max() {
-        return 0;
+        return max;
     }
 
     public Integer pop() {
-        return 0;
+        if (stack.size()==0) {
+            return null;
+        }
+        Integer x = stack.get(stack.size()-1);
+        stack.remove(stack.size()-1);
+        if (stack.size() > 0 && x == max())
+            max = Collections.max(stack);
+        else if (stack.size() == 0)
+            max = 0;
+        return x;
     }
 
     public void push(Integer x) {
-
+        if (x > max())
+            max = x;
+        stack.add(x);
     }
 }
