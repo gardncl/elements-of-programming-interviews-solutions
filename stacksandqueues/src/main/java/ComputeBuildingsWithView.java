1c6134b4a326a69597c9178104a8bff92e69cbdd
gardncl <gardncl@gmail.com>
2017-05-15 19:51:40 -0400

    9.6 done

--- stacksandqueues/src/main/java/ComputeBuildingsWithView.java
+++ stacksandqueues/src/main/java/ComputeBuildingsWithView.java
@@ -1,3 +1,4 @@
+import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -9,7 +10,15 @@ public class ComputeBuildingsWithView {
     */
 
     public static Deque<BuildingWithHeight> examineBuildingsWithSunset(Iterator<Integer> sequence) {
-
-        return new LinkedList<>();
+        Deque<BuildingWithHeight> stack = new ArrayDeque<>();
+        int c = 0;
+        while (sequence.hasNext()) {
+            Integer temp = sequence.next();
+            while (stack.peekFirst()!=null && temp >= stack.peekFirst().height) {
+                stack.removeFirst();
+            }
+            stack.addFirst(new BuildingWithHeight(c++,temp));
+        }
+        return stack;
     }
 }
