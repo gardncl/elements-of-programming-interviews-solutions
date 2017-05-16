c4c385856b79895e48d7a353b6df8ab64aa48af0
gardncl <gardncl@gmail.com>
2017-05-16 18:56:23 -0400

    13.2 done

--- hashtables/src/main/java/IsLetterConstructable.java
+++ hashtables/src/main/java/IsLetterConstructable.java
@@ -1,3 +1,5 @@
+import java.util.Hashtable;
+
 public class IsLetterConstructable {
 
     /*
@@ -5,7 +7,20 @@ public class IsLetterConstructable {
     */
 
     public static boolean isConstructable(String letterText, String magazineText) {
+        Hashtable<Character, Integer> letters = new Hashtable<>();
+        for (Character c : magazineText.toLowerCase().toCharArray()) {
+            if (!letters.containsKey(c))
+                letters.put(c,1);
+            else
+                letters.put(c, letters.get(c)+1);
+        }
 
-        return false;
+        for (Character c  : letterText.toLowerCase().toCharArray()) {
+            if (!letters.containsKey(c) || letters.get(c) == 0)
+                return false;
+            else
+                letters.put(c, letters.get(c)-1);
+        }
+        return true;
     }
 }
