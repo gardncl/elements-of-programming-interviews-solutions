c2c0907f9d1d4369541e4c57caac6c2b6733aaec
gardncl <gardncl@gmail.com>
2017-05-17 19:01:45 -0400

    9.2 done

--- stacksandqueues/src/main/java/EvaluateRPNExpressions.java
+++ stacksandqueues/src/main/java/EvaluateRPNExpressions.java
@@ -1,3 +1,7 @@
+import com.sun.tools.javac.util.StringUtils;
+
+import java.util.Stack;
+
 public class EvaluateRPNExpressions {
 
     /*
@@ -5,7 +9,34 @@ public class EvaluateRPNExpressions {
     */
 
     public static Integer eval(String RPNExpression) {
+        Stack<Integer> stack = new Stack<>();
+        int a, b;
+        for (String s : RPNExpression.split(",")) {
+            if (s.length() > 1 &&
+                    s.charAt(0) == '-' ? s.substring(1).trim().chars().allMatch(Character::isDigit)
+                                        : s.trim().chars().allMatch(Character::isDigit))
+                stack.push(Integer.parseInt(s));
+            else {
+                try {
+                    a = stack.pop();
+                    b = stack.pop();
+                } catch (Exception e) {
+                    return null;
+                }
+                switch (s) {
+                    case "-":   stack.push(a - b);
+                                break;
+                    case "+":   stack.push(a + b);
+                                break;
+                    case "/":   stack.push(a / b);
+                                break;
+                    case "x":   stack.push(a * b);
+                                break;
+                    default:    return null;
+                }
 
-        return 0;
+            }
+        }
+        return stack.pop();
     }
 }
