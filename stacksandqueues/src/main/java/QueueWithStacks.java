becdb61f2532b26dfa95382244bf3deced34ec83
gardncl <gardncl@gmail.com>
2017-05-18 19:43:54 -0400

    9.9 done

--- stacksandqueues/src/main/java/QueueWithStacks.java
+++ stacksandqueues/src/main/java/QueueWithStacks.java
@@ -1,18 +1,38 @@
+import java.util.Stack;
+
 public class QueueWithStacks {
 
     /*
     9.9
     */
 
+    private Stack<Integer> push = new Stack<>();
+    private Stack<Integer> pop = new Stack<>();
+
     public QueueWithStacks() {
     }
 
     public void enqueue(Integer x) {
-
+        if (!pop.isEmpty())
+            preparePush();
+        push.push(x);
     }
 
     public Integer dequeue() {
+        if (!push.isEmpty())
+            preparePop();
+        return pop.pop();
+    }
 
-        return 0;
+    private void preparePush() {
+        while (!pop.isEmpty()) {
+            push.push(pop.pop());
+        }
+    }
+
+    private void preparePop() {
+        while (!push.isEmpty()) {
+            pop.push(push.pop());
+        }
     }
 }
