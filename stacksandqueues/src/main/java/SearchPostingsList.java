6c1f424d63df89432803b53226aa2125203105e7
gardncl <gardncl@gmail.com>
2017-05-21 17:17:25 -0400

    9.5 recursive solved and changed tests to simpler implementations

--- stacksandqueues/src/main/java/SearchPostingsList.java
+++ stacksandqueues/src/main/java/SearchPostingsList.java
@@ -5,7 +5,16 @@ public class SearchPostingsList {
     */
 
     public static void setJumpOrderRecursive(PostingListNode<Integer> node) {
+        setJumpOrderRecursive(node, 0);
+    }
 
+    private static int setJumpOrderRecursive(PostingListNode<Integer> node, int order) {
+        if (node != null && node.data == -1) {
+            node.data = order++;
+            order = setJumpOrderRecursive(node.jump, order);
+            order = setJumpOrderRecursive(node.next, order);
+        }
+        return order;
     }
 
     public static void setJumpOrderIterative(PostingListNode<Integer> node) {
