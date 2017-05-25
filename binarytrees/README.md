0665ec69ce5896212edf1f5b36a180ed7436fa3a
gardncl <gardncl@gmail.com>
2017-05-24 21:36:34 -0400

    Solved 10.7, added attribute ot BinaryTree fro visted, and fixed 10.7 tests

--- binarytrees/README.md
+++ binarytrees/README.md
@@ -6,7 +6,7 @@
 - [x] 10.4 ComputeLCAWithParent
 - [x] 10.5 SumRootToLeaf
 - [x] 10.6 FindRootToLeafSum
-- [ ] 10.7 InorderIterative
+- [x] 10.7 InorderIterative
 - [ ] 10.8 PreorderIterative
 - [ ] 10.9 ComputeKthNodeInorder
 - [ ] 10.10 ComputeSuccessor
--- binarytrees/src/main/java/InorderIterative.java
+++ binarytrees/src/main/java/InorderIterative.java
@@ -1,5 +1,6 @@
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class InorderIterative {
 
@@ -7,8 +8,22 @@ public class InorderIterative {
     10.7
     */
 
-    public static List<Integer> BSTInOrder(BinaryTree<Integer> tree) {
-
-        return Collections.emptyList();
+    public static List<Integer> BSTInOrder(BinaryTree<Integer> node) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Stack<BinaryTree<Integer>> stack = new Stack<>();
+        while (!node.isVisited || !stack.isEmpty()) {
+            if (node.isVisited)
+                node = stack.pop();
+            if (node.left != null && !node.left.isVisited) {
+                stack.push(node);
+                node = node.left;
+            } else {
+                list.add(node.data);
+                node.isVisited = true;
+                if (node.right != null)
+                    node = node.right;
+            }
+        }
+        return list;
     }
 }
