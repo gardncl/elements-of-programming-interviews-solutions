d9cc46bba3daf7091c97411305a4a190de62c6be
gardncl <gardncl@gmail.com>
2017-05-26 17:41:05 -0400

    10.10 solved, binary tree changed

--- binarytrees/src/main/java/ComputeSuccessor.java
+++ binarytrees/src/main/java/ComputeSuccessor.java
@@ -4,8 +4,20 @@ public class ComputeSuccessor {
     10.10
     */
 
-    public static BinaryTree<Integer> findSuccessor(BinaryTree<Integer> node) {
+    public static BinaryTreeParent<Integer> findSuccessor(BinaryTreeParent<Integer> node) {
+        if (node.right != null) {
+            node = node.right;
+            while (node.left != null)
+                node = node.left;
+            return node;
+        }
 
-        return new BinaryTree<Integer>(0);
+        if (node.parent != null && node.equals(node.parent.left))
+            return node.parent;
+
+        while (node.parent != null && node.equals(node.parent.right))
+            node = node.parent;
+
+        return node.parent;
     }
 }
