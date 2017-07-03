31f2f30c720f258a4815913e23f4a3e133a65730
gardncl <gardncl@gmail.com>
2017-07-03 16:51:21 -0400

    changed test and solution for sort almost sorted

--- heaps/src/main/java/SortIncDec.java
+++ heaps/src/main/java/SortIncDec.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class SortIncDec {
 
@@ -8,7 +10,25 @@ public class SortIncDec {
     */
 
     public static List<Integer> sort(List<Integer> list) {
+        List<PriorityQueue<Integer>> priorityQueueList = new ArrayList<>();
+        if (list.size() < 2) return list;
 
-        return Collections.emptyList();
+        boolean increasing = list.get(0) < list.get(1);
+        int e = 0;
+        priorityQueueList.add(new PriorityQueue<>(Integer::compareTo));
+
+        for (int i = 1; i < list.size(); i++) {
+            if (increasing == (list.get(i-1) < list.get(i))) {
+                priorityQueueList.get(e).offer(list.get(i));
+            }
+        }
+
+
+        //PUT PRIORITY QUEUES INTO LIST OF LISTS TO BE PROCESSED BY THE SOLUTION TO 11.1
+        List<List<Integer>> result = new ArrayList<>();
+        for (PriorityQueue<Integer> pq : priorityQueueList) {
+            result.add(new ArrayList<>(pq));
+        }
+        return MergeSortedFiles.mergeSorted(result);
     }
 }
