47325e5b12ffa48fb03d39639f278a91551afc1c
gardncl <gardncl@gmail.com>
2017-08-20 12:19:17 -0400

    change to 5.9 because of pull request https://github.com/gardncl/elements-of-programming-interviews/pull/8

--- primitives/src/main/java/CheckIfIntegerIsPalindrome.java
+++ primitives/src/main/java/CheckIfIntegerIsPalindrome.java
@@ -5,6 +5,10 @@ public class CheckIfIntegerIsPalindrome {
     */
 
     public static boolean isPalindrome(int x) {
+        //FROM BOOK SOLUTION
+        if (x < 0)
+            return false;
+
         int i = 0, j = 31;
         for (int k = 0; k <= 16; k++) {
             if (((x >>> i++) & 1) != ((x >>> j--) & 1)) {
